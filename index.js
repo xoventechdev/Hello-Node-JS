58d44a2852a3c5a0e508dda1ade526efe615c3a6
xoventechdev <alkarim.doha@gmail.com>
2024-01-23 17:26:41 +0300

    Used url module

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var http = require('http');
+var url = require('url');
 
 http.createServer(function(req, res) {
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    const weburl = 'https://www.facebook.com/groups/1013044613268884';
 
-}).listen(8080);
+    // PathName 1
+    const parseUrl = url.parse(weburl);
+    const pathname = parseUrl.pathname;
+
+    // PathName 2
+    const pathName = url.parse(weburl).pathname;
+
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(`PathName 1 : ${pathname} <br> PathName 2 : ${pathName}`);
+
+}).listen(3030);
 
